f7fb684891dfd7ec4dddd7242f74d739adacd246
Sylvain <sylvain.laurent123@sfr.fr>
2026-01-27 12:31:16 +0100

    Third commit

---
+++ db.js
@@ -0,0 +1,34 @@
+// Il permet de configurer le pool de connexions à MySQL
+// mysql2 pour faire des requêtes asynchrones async/await
+
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+// Pool de connexions, permet de :
+// Gérer plusieurs connexions simultanées
+// Réutiliser des connexions existantes
+// Gestion automatique de la disponiilité
+// Limiter le nombre de connexions simultanées
+
+const db = mysql.createPool({
+    // Paramètres de connexion (host, nom d'utilisateur, mdp, nom bdd)
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
+    // Paramètre du pool
+    // Si plus de connexion dispo, alors elles attendent
+    waitForConnections: true,
+    // Limiter le nombre max de connexions
+    connectionLimit: 10,
+
+    // Paramètres optionnels mais recommandés
+    // En cas d'échec de co, réessayer
+    enableKeepAlive: true,
+    KeepAliveInitialDelay: 0,
+    // timeout de connexion (millisecondes)
+    connectTimeout: 10000, // 10s
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-require('dotenv').config(); // Charge le .env
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+require('dotenv').config(); // Sert à charger les var d'en depuis .env
+
+//Connexion à la bdd (base de données)
+const db = require('./db');
+
+//Importation des routes à venir
+
+//Création à l'application Express
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Bonjour API CafThé !');
+//Middlewares
+// Parser les JSON
+app.use(express.json());
+
+// Logger de requêtes HTTP dans la console
+app.use(morgan("dev"));
+
+// Permet les requêtes cross-origin (qui viennent du front)
+// CORS = Cross-Origin Ressource Sharing
+// Obligatoire sinon le navigateur bloque les requêtes
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173 ',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}),
+);
+
+// ROUTES
+
+// Route de test pour vérifier que l'api fonctionne
+app.get("/health", (req, res) => {
+    res.json({
+        status: "OK",
+        mesage: "API is running",
+    })
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+// Routes de l'API
+// ... à venir
+
+// Gestion des erreurs
+// Route 404
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Road not Found',
+    })
+});
+
+// Démarrage du serveur
+const port = process.env.PORT || 3000;
+const host = process.env.Host || "localhost";
+
+app.listen(port, host, () => {
+    console.log(`Serveur démarré sur http://${host}:${port}`);
 });
