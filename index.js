841cefc044ff6103508e3189cb915d3f74e191c6
Sylvain <sylvain.laurent123@sfr.fr>
2026-01-27 11:03:58 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-require('dotenv').config();
-
+require('dotenv').config(); // Charge le .env
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bonjour API CafThé !');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
